cc957e49bcaa27574b62182046dac87f63d8f896
Cutina Woof <gretchenista@gmail.com>
2018-03-01 20:16:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine (line) {
   var i = 0;
  if (line.length > 0) {
    line.push ("The line is currently " + [i] + ". " + line[i] + ", ");
-  
+       i++;
  } else {
     return("The line is currently empty.");
   }
